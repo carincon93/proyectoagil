299268030a8a34f34aa3a0eaa74cc6eac6991487
carincon93 <carincon93@gmail.com>
2017-03-13 10:00:02 -0500

    proyectoagil

---
+++ dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <div class="navigation">
+    	<nav>
+    		<a href="" class="" data-toggle="collapse" href="#" data-target="#aside-nav" aria-expanded="false" aria-controls="aside-nav">Vehiculos</a>
+    		<ul class="collapse" id="aside-nav" aria-expanded="false">
+    			<li>
+    				<a href="">a</a>
+    			</li>
+    			<li>
+    				<a href="">b</a>
+    			</li>
+    			<li>
+    				<a href="">c</a>
+    			</li>
+    		</ul>
+    	</nav>
+    </div>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
