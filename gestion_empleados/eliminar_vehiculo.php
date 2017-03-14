5915949ea1e15e5ad03b4693c00a40bb972f520c
paolasanchezq <cindypsq@hotmail.es>
2017-03-14 12:30:58 -0500

    gestion empleados

---
+++ gestion_empleados/eliminar_vehiculo.php
@@ -0,0 +1,13 @@
+<?php
+include "../layouts/conexion.php";
+if ($_GET) {
+	$id = $_GET['id'];
+	$row = mysqli_query($con, "DELETE FROM gestion_vehiculos_tbl WHERE id_vehiculos = $id");
+	if ($row) {
+	echo "<script>
+		alert('usuario eliminado con exito');
+		window.location.replace('vehiculos.php');
+	</script>";
+	}
+}
+?>
