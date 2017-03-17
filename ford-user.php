48de4860e419a2382a651fdcc0c52e94ef9b5b60
carincon93 <carincon93@gmail.com>
2017-03-17 07:44:08 -0500

    proyectoagil

---
+++ ford-user.php
@@ -0,0 +1,30 @@
+<?php $page = "ford"; ?>
+<?php include 'layouts/conexion.php' ?>
+<?php 
+    $query = mysqli_query($con, "SELECT * FROM gestion_vehiculos_tbl WHERE marca = 'ford' ");
+?>
+<?php include 'layouts/header.php'; ?>
+<?php include 'layouts/navbar-user.php' ?>
+    <div class="content">
+        <div class="container">
+            <ul class="row">
+                <?php while ($row = mysqli_fetch_array($query)): ?>
+                <li class="col-md-4 offset-md-1 col-sm-6 offset-sm-3 col-10 offset-1 vehicle-card">
+                    <div class="card-title">
+                        <h1 class="marca-title"><?= $row['marca'] ?></h1>
+                        <h2 class="modelo-title"><?= $row['linea'] ?></h2>
+                    </div>
+                    <div>
+                        <div class="img-card">
+                            <img src="imgs/<?= $row['imagen'] ?>" width="100%">
+                        </div>
+                        <p class="card-description"><?= $row['descripcion']  ?></p>
+                        <div class="actions">
+                            <button type="button" class="btn btn-primary icon-cart" data-toggle="modal" data-target="#myModal"></button>
+                        </div>
+                    </div>
+                </li>
+                <?php endwhile; ?>
+        </div>
+    </div>
+<?php include 'layouts/footer.php'; ?>
---
+++ mazda-user.php
@@ -0,0 +1,65 @@
+<?php $page = "mazda"; ?>
+<?php include 'layouts/conexion.php' ?>
+<?php 
+    $query = mysqli_query($con, "SELECT * FROM gestion_vehiculos_tbl WHERE marca = 'mazda' ");
+?>
+<?php include 'layouts/header.php'; ?>
+<?php include 'layouts/navbar-user.php' ?>
+    <div class="content">
+        <div>            
+            <ul>
+                <?php while ($row = mysqli_fetch_array($query)): ?>
+                <li class="col-md-4 offset-md-1 col-sm-6 offset-sm-3 col-10 offset-1 vehicle-card">
+                    <div class="card-title">
+                        <h1 class="marca-title"><?= $row['marca'] ?></h1>
+                        <h2 class="modelo-title"><?= $row['linea'] ?></h2>
+                    </div>
+                    <div>
+                        <div class="img-card">
+                            <img src="imgs/<?= $row['imagen'] ?>" width="100%">
+                        </div>
+                        <p class="card-description"><?= $row['descripcion']  ?></p>
+                        <div class="actions">
+                            <button type="button" class="btn btn-primary icon-cart" data-toggle="modal" data-target="#myModal"></button>
+                        </div>
+                    </div>
+                </li>
+                <?php endwhile ?>       
+            </ul>
+        </div>        
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <table>
+                <thead>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Precio</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>CX-3</td>
+                        <td>2017</td>
+                        <td id=""><?= $vehiculo['precio'] ?></td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+<?php include 'layouts/footer.php'; ?>
